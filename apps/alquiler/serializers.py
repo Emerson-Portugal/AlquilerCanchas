4dc40d7399ec8d2078241c408305954652a1ff90
emersoncrp <emersoncrp@gmail.com>
2024-07-02 20:57:08 -0500

    terminado2

---
+++ apps/alquiler/serializers.py
@@ -0,0 +1,43 @@
+# serializers.py
+from rest_framework import serializers
+from .models import Local, Cancha, Horario, AccesoriosProducto, ReservaCancha, DetalleReserva
+
+class LocalSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Local
+        fields = '__all__'
+
+class CanchaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cancha
+        fields = '__all__'
+
+class HorarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Horario
+        fields = '__all__'
+
+class AccesoriosProductoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = AccesoriosProducto
+        fields = '__all__'
+
+class ReservaCanchaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservaCancha
+        fields = '__all__'
+
+class DetalleReservaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleReserva
+        fields = '__all__'
+
+class ReservaCanchaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReservaCancha
+        fields = '__all__'
+
+class DetalleReservaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = DetalleReserva
+        fields = '__all__'
